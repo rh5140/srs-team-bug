7caf81a8bd3ef5764e0fac4b9b4b8bfa6b87989c
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2022-01-23 11:26:05 -0800

    Add folders

---
+++ DebuggerGame/Assets/Scripts/Board.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for inputs
+    }
+
+    // Turn structure
+
+    // Movement 
+    // Keep track of locations of all entities
+}
