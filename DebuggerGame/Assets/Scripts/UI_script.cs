4ed7a0513d5633e92f5c89a5dcf478fa439e1ec6
HiccupHan <hanxuanzhe2002@gmail.com>
2022-01-24 17:26:06 -0800

    basic main menu
    Basic main menu with functioning start (links to level 1 scene) and quit buttons and a C# script specifically for UI

---
+++ DebuggerGame/Assets/Scripts/UI_script.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void play_game()
+    {
+        SceneManager.LoadScene("level 1");
+    }
+
+    public void quit_game()
+    {
+        Application.Quit();
+    }
+}
