ca2085eb15ee2cc65d3cf62db3a846d7c99c9d65
Brad <bradlowe@g.ucla.edu>
2022-02-22 14:03:31 -0800

    StationaryArthropod = Arthropod

--- DebuggerGame/Assets/Scripts/StationaryArthropod.cs
+++ DebuggerGame/Assets/Scripts/StationaryArthropod.cs
@@ -5,28 +5,5 @@ using UnityEngine;
 public class StationaryArthropod : Arthropod
 {
 
-    BoardAction RuleMap(BoardAction action)
-    {
-        return new NullAction(action.boardObject);
-    }
-
-    protected override void Start()
-    {
-        base.Start();
-        AddActionRule(
-            new EFMActionRule(
-                this,
-                board,
-                enableConditions: new List<EFMActionRule.EnableCondition> {
-                    (BoardObject creator, Board board)
-                    => creator is Arthropod arthropod && !arthropod.isCaught && arthropod.rulesEnabled
-                },
-
-                filter: (BoardAction action) =>
-                    action.boardObject is Player
-                    && action is MovementAction movementAction,
-                map: RuleMap
-            )
-        );
-    }
+    
 }
