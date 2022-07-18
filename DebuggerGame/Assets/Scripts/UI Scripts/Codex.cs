0d482aca7f139dc250da23c4b69a0598b0de8b80
Andrew Zhu <andrewzhu1223@g.ucla.edu>
2022-07-17 18:39:37 -0700

    Implemented preliminary codex functionality
    Uses a HashSet to store the names of characters met. When a character is met, their name, description, and image are unhidden. Currently, "meet" characters by pressing the keys G-L.

--- DebuggerGame/Assets/Scripts/UI Scripts/Codex.cs
+++ DebuggerGame/Assets/Scripts/UI Scripts/Codex.cs
@@ -1,21 +1,129 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Codex : MonoBehaviour
 {
+    public static Codex instance = null;
+
     public static bool codexOpen = false;
     public GameObject codexDisplay;
     public GameObject denizensDisplay;
     public GameObject bugsDisplay;
 
+    //Denizen entries
+    private GameObject elderFlytrapEntry;
+    private GameObject cryingCloverEntry;
+    private GameObject flyKingEntry;
+
+    //Enemy entries
+    private GameObject basicFlyEntry;
+    private GameObject weirdFlyEntry;
+
+    //Codex
+    private HashSet<string> codex = new HashSet<string>();
+
     // Start is called on the frame when a script is enabled just before any of the Update methods are called the first time.
-    // void Start() {}
+    void Start() 
+    {
+        //Find denizen entries
+        elderFlytrapEntry = denizensDisplay.transform.Find("Scroll View/Viewport/Content/Elder Flytrap Entry").gameObject;
+        cryingCloverEntry = denizensDisplay.transform.Find("Scroll View/Viewport/Content/Crying Clover Entry").gameObject;
+        flyKingEntry = denizensDisplay.transform.Find("Scroll View/Viewport/Content/Fly King Entry").gameObject;
+
+        //Find enemy entries
+        basicFlyEntry = bugsDisplay.transform.Find("Scroll View/Viewport/Content/Basic Fly Entry").gameObject;
+        weirdFlyEntry = bugsDisplay.transform.Find("Scroll View/Viewport/Content/Weird Fly Entry").gameObject;
+    }
+
     // Update is called every frame, if the MonoBehaviour is enabled.
-    // void Update() {}
+    void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            AddToCodex("elderFlytrap");
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            AddToCodex("cryingClover");
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            AddToCodex("flyKing");
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            AddToCodex("basicFly");
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            AddToCodex("weirdFly");
+        }
+    }
+
+    private void Awake()
+    {
+        //Singleton pattern
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
 
     // Functionality methods
-    // (Andrew write your code here, can delete this line after you've read this)
+
+    public void AddToCodex(string s)
+    {
+        codex.Add(s);
+        UnhideCharacter(s);
+    }
+
+    public void UnhideCharacter(string s)
+    {
+
+        switch (s)
+        {
+            case "elderFlytrap":
+                elderFlytrapEntry.transform.Find("Image").gameObject.GetComponent<Image>().color = Color.white;
+                elderFlytrapEntry.transform.Find("Name").gameObject.GetComponent<TextMeshProUGUI>().text = "Elder Flytrap";
+                elderFlytrapEntry.transform.Find("Description").gameObject.GetComponent<TextMeshProUGUI>().text = "[INSERT DESCRIPTION]";
+                break;
+            
+            case "cryingClover":
+                cryingCloverEntry.transform.Find("Image").gameObject.GetComponent<Image>().color = Color.white;
+                cryingCloverEntry.transform.Find("Name").gameObject.GetComponent<TextMeshProUGUI>().text = "Crying Clover";
+                cryingCloverEntry.transform.Find("Description").gameObject.GetComponent<TextMeshProUGUI>().text = "[INSERT DESCRIPTION]";
+                break;
+            
+            case "flyKing":
+                flyKingEntry.transform.Find("Image").gameObject.GetComponent<Image>().color = Color.white;
+                flyKingEntry.transform.Find("Name").gameObject.GetComponent<TextMeshProUGUI>().text = "Fly King";
+                flyKingEntry.transform.Find("Description").gameObject.GetComponent<TextMeshProUGUI>().text = "[INSERT DESCRIPTION]";
+                break;
+
+            case "basicFly":
+                basicFlyEntry.transform.Find("Image").gameObject.GetComponent<Image>().color = Color.white;
+                basicFlyEntry.transform.Find("Name").gameObject.GetComponent<TextMeshProUGUI>().text = "Basic Fly";
+                basicFlyEntry.transform.Find("Description").gameObject.GetComponent<TextMeshProUGUI>().text = "[INSERT DESCRIPTION]";
+                break;
+            
+            case "weirdFly":
+                weirdFlyEntry.transform.Find("Image").gameObject.GetComponent<Image>().color = Color.white;
+                weirdFlyEntry.transform.Find("Name").gameObject.GetComponent<TextMeshProUGUI>().text = "Weird Fly";
+                weirdFlyEntry.transform.Find("Description").gameObject.GetComponent<TextMeshProUGUI>().text = "[INSERT DESCRIPTION]";
+                break;
+
+            default:
+                break;
+        }
+    }
 
     // UI methods
 
